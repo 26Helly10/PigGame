d6d33e63ab143cf92dafcb33f5e264129decfb36
Elena <53377385+26Helly10@users.noreply.github.com>
2023-08-02 08:11:34 +0300

    the initialization of the beginning of the game

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//Selecting elements
+const scorePlayer0 = document.getElementById('score--0');
+const scorePlayer1 = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+scorePlayer0.textContent = 0;
+scorePlayer1.textContent = 0;
+diceEl.classList.add('hidden');
